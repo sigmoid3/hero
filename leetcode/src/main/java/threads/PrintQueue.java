a227a977eb254c8a52e7227b4710baf83e7eeea3
qmq <minqianqiang@gmail.com>
2020-03-22 17:36:44 +0800

    update

---
+++ leetcode/src/main/java/threads/PrintQueue.java
@@ -0,0 +1,70 @@
+package threads;
+
+/**
+ * @Author: minqian
+ * @Create: 2020/3/21
+ * @Description: 我们提供了一个类：
+ * public class Foo {
+ * public void one() { print("one"); }
+ * public void two() { print("two"); }
+ * public void three() { print("three"); }
+ * }
+ * 三个不同的线程将会共用一个 Foo 实例。
+ * <p>
+ * 线程 A 将会调用 one() 方法
+ * 线程 B 将会调用 two() 方法
+ * 线程 C 将会调用 three() 方法
+ * 请设计修改程序，以确保 two() 方法在 one() 方法之后被执行，three() 方法在 two() 方法之后被执行。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: "onetwothree"
+ * 解释:
+ * 有三个线程会被异步启动。
+ * 输入 [1,2,3] 表示线程 A 将会调用 one() 方法，线程 B 将会调用 two() 方法，线程 C 将会调用 three() 方法。
+ * 正确的输出是 "onetwothree"。
+ * 示例 2:
+ * <p>
+ * 输入: [1,3,2]
+ * 输出: "onetwothree"
+ * 解释:
+ * 输入 [1,3,2] 表示线程 A 将会调用 one() 方法，线程 B 将会调用 three() 方法，线程 C 将会调用 two() 方法。
+ * 正确的输出是 "onetwothree"。
+ * <p>
+ * <p>
+ * 注意:
+ * <p>
+ * 尽管输入中的数字似乎暗示了顺序，但是我们并不保证线程在操作系统中的调度顺序。
+ * <p>
+ * 你看到的输入格式主要是为了确保测试的全面性。
+ **/
+public class PrintQueue {
+}
+
+class Foo {
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
