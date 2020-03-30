41c61b19304296a8366799b81864cdf657c8cece
qmq <minqianqiang@gmail.com>
2020-03-31 00:01:03 +0800

    update

--- leetcode/src/main/java/threads/PrintQueue.java
+++ leetcode/src/main/java/threads/PrintQueue.java
@@ -50,21 +50,23 @@ class Foo {
 
     }
 
-    public void first(Runnable printFirst) throws InterruptedException {
+    volatile int count = 1;
 
-        // printFirst.run() outputs "first". Do not change or remove this line.
+    public void first(Runnable printFirst) throws InterruptedException {
         printFirst.run();
+        count++;
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-
-        // printSecond.run() outputs "second". Do not change or remove this line.
+        while (count != 2) {
+        }
         printSecond.run();
+        count++;
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-
-        // printThird.run() outputs "third". Do not change or remove this line.
+        while (count != 3) {
+        }
         printThird.run();
     }
 }
