eccd7a2f4ba9bbe4943653df2ee49b7a81b0a50b
minqian <minqianqiang@gmail.com>
2020-07-09 14:56:58 +0800

    .

---
+++ base/src/main/java/improve/grammar/BaseSean.java
@@ -0,0 +1,62 @@
+package improve.grammar;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Stack;
+import java.util.Vector;
+
+/**
+ * @author: minqian
+ * @since: 2020/7/9
+ * @description:
+ */
+public class BaseSean {
+
+    public static void main(String[] args) {
+        enumTest();
+        stackTest();
+        hashTableTest();
+    }
+
+    /**
+     * 包含了一种类似C++的向量类型
+     */
+    public static void enumTest() {
+        Enumeration<String> enumeration;
+        Vector<String> names = new Vector<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        enumeration = names.elements();
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
+    }
+
+    /**
+     * 简单栈
+     */
+    public static void stackTest() {
+        Stack<String> stack = new Stack<>();
+        if (stack.empty()) {
+            stack.push("a");
+            stack.push("b");
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    /**
+     * hash表哦
+     */
+    public static void hashTableTest() {
+        Hashtable<String,Object> names = new Hashtable<>();
+        names.put("a", 1);
+        names.put("b", 2);
+        names.put("c", 3);
+        System.out.println(names.get("b"));
+        names.clear();
+        System.out.println(names.isEmpty());
+    }
+}
